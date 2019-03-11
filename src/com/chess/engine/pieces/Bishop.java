aff15b0829287d289e4760ecd7fe44eae22f7eba
Maanav Garg <maanavgarg@icloud.com>
2019-03-11 03:58:19 -0400

    Bishop Class: Vectors added

--- src/com/chess/engine/pieces/Bishop.java
+++ src/com/chess/engine/pieces/Bishop.java
@@ -2,7 +2,10 @@ package com.chess.engine.pieces;
 
 import com.chess.engine.Alliance;
 import com.chess.engine.board.Board;
+import com.chess.engine.board.BoardUtils;
 import com.chess.engine.board.Move;
+import com.chess.engine.board.Tile;
+import com.google.common.collect.ImmutableList;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -23,16 +26,32 @@ public class Bishop extends Piece {
         final List<Move> legalMoves = new ArrayList<>();
 
         for (final int candidateCoordinateOffset: CANDIDATE_MOVE_VECTOR_COORDINATES) {
-            
+
+            int candidateDestinationCoordinate = this.piecePosition;
+
+            while (BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate)) {
+
+                candidateDestinationCoordinate += candidateCoordinateOffset;
+
+                if(BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate)) {
+                    final Tile candidateDestinationTile = board.getTile(candidateDestinationCoordinate);
+
+                    if(!candidateDestinationTile.isTileOccupied()){
+                        legalMoves.add(new Move.MajorMove(board, this, candidateDestinationCoordinate));
+                    } else {
+                        final Piece pieceAtDestination = candidateDestinationTile.getPiece();
+                        final Alliance pieceAlliance = pieceAtDestination.getPieceAlliance();
+                        if(this.pieceAlliance != pieceAlliance) {
+                            legalMoves.add(new Move.AttackMove(board, this, candidateDestinationCoordinate, pieceAtDestination));
+                        }
+                    }
+                    break;
+
+                }
+            }
+
         }
 
-
-
-
-
-
-
-
-        return null;
+        return ImmutableList.copyOf(legalMoves);
     }
 }
